8bb9c4bde3e4848cb1a70f135c40c1023b862c4a
Nadina <minadin.m@gmail.com>
2025-01-27 21:03:13 +0500

    Initial Commit

---
+++ ImageFeedExtendTests/ImageFeedExtendTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedExtendTests.swift
+//  ImageFeedExtendTests
+//
+//  Created by Nadin on 27.01.2025.
+//
+
+import Testing
+@testable import ImageFeedExtend
+
+struct ImageFeedExtendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
